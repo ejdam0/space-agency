e10f009aaabc6e9863cd7e95409bd87229b85d82
ejdam0 <adam.strzelecki0@gmail.com>
2020-03-31 21:45:20 +0200

    Cleanup
    Signed-off-by: ejdam0 <adam.strzelecki0@gmail.com>

--- src/main/java/pl/strzelecki/spaceagency/config/WebConfig.java
+++ src/main/java/pl/strzelecki/spaceagency/config/WebConfig.java
@@ -25,22 +25,13 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Bean
     public Docket api() {
-        // @formatter:off
-        //Register the controllers to swagger
-        //Also it is configuring the Swagger Docket
         return new Docket(DocumentationType.SWAGGER_2).select()
-                // .apis(RequestHandlerSelectors.any())
                 .apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
-                // .paths(PathSelectors.any())
-                // .paths(PathSelectors.ant("/swagger2-demo"))
                 .build();
-        // @formatter:on
     }
 
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry)
-    {
-        //enabling swagger-ui part for visual documentation
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
